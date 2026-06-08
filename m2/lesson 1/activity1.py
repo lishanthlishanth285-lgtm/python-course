7653af61165a40a695ea5c0d15f0e5efcacd8a5d
lishanthlishanth285-lgtm <lishanthlishanth285#gmail.com>
2026-06-08 11:45:12 +0400

    codingal

---
+++ m2/lesson 1/activity1.py
@@ -0,0 +1,23 @@
+# 1) Ask the student if they had a medical cause and store the answer in `medical_cause`.
+#    (Also clean the input so it becomes either 'Y' or 'N'.)
+
+# 2) If `medical_cause` is 'Y':
+#    - Print that the student is allowed to attend the exam.
+
+# 3) Otherwise (medical_cause is 'N'):
+#    a) Ask for the student’s attendance percentage and store it in `atten`.
+#    b) If `atten` is 75 or more:
+#       - Print "Allowed"
+#    c) Else:
+#       - Print "Not allowed"
+
+medical_cause = input("Did you have a medical cause? (Y/N): ")
+
+if medical_cause == 'Y':
+    print("You are allowed to attend the exam.")
+else:
+    atten = int(input("What is your attendance percentage? "))
+    if atten >= 75:
+        print("Allow to write exam")
+    else:
+        print("Not allowed to write exam")
---
+++ m2/projects/project1.py
@@ -0,0 +1,5 @@
+age = input("What is your age? ")
+if age >= 10 and age <= 20:
+    print("You are allowed to enter this class")
+else:   
+    print("You are not allowed to enter this class")
