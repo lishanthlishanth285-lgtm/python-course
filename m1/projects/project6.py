31f8a55404e3323c3a96f17214af1f079bcb95e5
lishanthlishanth285-lgtm <lishanthlishanth285#gmail.com>
2026-06-11 22:47:45 +0400

    alphabat checer

---
+++ m1/projects/project6.py
@@ -0,0 +1,7 @@
+#checking the input is a alphabat or not
+
+a = input("enter a character")
+if a == str(a):
+    print("The input is an alphabet.")
+else:
+    print("The input is not an alphabet.")
