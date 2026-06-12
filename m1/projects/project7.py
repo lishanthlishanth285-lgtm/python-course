315e90bc86f730d923fb3511d1c417bdff8d5dab
lishanthlishanth285-lgtm <lishanthlishanth285#gmail.com>
2026-06-12 14:34:36 +0400

    codingal

---
+++ m1/projects/project7.py
@@ -0,0 +1,8 @@
+a = 1
+b = 2
+c = 3
+print(f"a={a},b={b},c={c}")
+#a,b,c = c,a,b
+a,b,c = b,c,a
+print("after swaping")
+print(f"a={a},b={b},c={c}")
---
+++ m2/lesson 3/activity1.py
@@ -0,0 +1,9 @@
+i = 1
+sum = 0
+n = int(input("enter ur number:"))
+print("i=",end=" ")
+while i <= n:
+    sum = sum+i
+    print(i,end=" ")
+    i = i+1
+print("\nsum",sum)
